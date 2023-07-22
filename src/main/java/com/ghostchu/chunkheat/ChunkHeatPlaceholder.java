9b57749b37251c543d0a48371e8832d6682a0589
T14D3 <sundmacher.tiade@gmail.com>
2023-07-22 16:29:42 +0200

    Reload command and PAPI Expansion

---
+++ src/main/java/com/ghostchu/chunkheat/ChunkHeatPlaceholder.java
@@ -0,0 +1,48 @@
+package com.ghostchu.chunkheat;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.Chunk;
+import org.bukkit.entity.Player;
+
+public class ChunkHeatPlaceholder extends PlaceholderExpansion {
+    private final ChunkHeat plugin;
+
+    public ChunkHeatPlaceholder(ChunkHeat plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean canRegister() {
+        return true;
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "chunkheat";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "T14D3"; // Replace with your name or plugin name
+    }
+
+    @Override
+    public String getVersion() {
+        return plugin.getDescription().getVersion();
+    }
+
+    @Override
+    public String onPlaceholderRequest(Player player, String identifier) {
+        if (player == null) {
+            return "";
+        }
+
+        if (identifier.equals("value")) {
+            Chunk chunk = player.getLocation().getChunk();
+            LimitEntry entry = plugin.chunkHeapMap.getIfPresent(chunk);
+            return String.valueOf(entry == null ? "0" : entry.getCounter().get());
+        }
+
+        return null;
+    }
+}
